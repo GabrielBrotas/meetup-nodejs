b2fab7b88f193a5dbd772fab986af2cd9168ec9c
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-08-17 21:09:24 -0300

    integrate http adapter

--- microservices/categories/packages/adapters/http/src/database/interfaces/db-config.interface.ts
+++ microservices/categories/packages/adapters/http/src/database/interfaces/db-config.interface.ts
@@ -1,15 +1,8 @@
-export interface IDatabaseConfigAttributes {
-  username?: string;
-  password?: string;
-  database?: string;
-  host?: string;
-  port?: number | string;
-  dialect?: string;
-  urlDatabase?: string;
-}
-
 export interface IDatabaseConfig {
-  development: IDatabaseConfigAttributes;
-  test: IDatabaseConfigAttributes;
-  production: IDatabaseConfigAttributes;
+  username: string;
+  password: string;
+  database: string;
+  host: string;
+  port: number;
+  dialect: string;
 }
--- microservices/categories/packages/adapters/http/src/main.ts
+++ microservices/categories/packages/adapters/http/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env.ENVIRONMENT);
+
   await app.listen(4001);
 }
 
